8498b49e578aa821622cb64c1f6ada065b1a5a87
Corbin <corbinlazarone1@gmail.com>
2025-09-21 11:30:14 -0500

    feat: user ip, request url and http method middleware logging for all request

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -2,6 +2,7 @@ package main
 
 import "net/http"
 
+// Add secure headers to all incoming requests
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
@@ -17,5 +18,14 @@ func secureHeaders(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 
 		// NOTE: Any code here will execute on the way back up the chain.
+	})
+}
+
+// Will log the IP address, URL, and http method of the user for every incoming request
+func (app *application) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+
+		next.ServeHTTP(w, r)
 	})
 }
