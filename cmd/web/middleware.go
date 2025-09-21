49de8012f57d14099463280edfd3af0b61cd5e0f
Corbin <corbinlazarone1@gmail.com>
2025-09-20 23:21:31 -0500

    feat: security headers - ex of CSP headersand middleware for all incoming requests

---
+++ cmd/web/middleware.go
@@ -0,0 +1,21 @@
+package main
+
+import "net/http"
+
+func secureHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
+
+		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("X-Frame-Options", "deny")
+		w.Header().Set("X-XSS-Protection", "0")
+
+		// NOTE: Any code here will execute on the way down the chain.
+		// Any early returns will break the chain.
+
+		next.ServeHTTP(w, r)
+
+		// NOTE: Any code here will execute on the way back up the chain.
+	})
+}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 	// Use the mux.Handle() function to register the file server as the handler for
 	// all URL paths that start with "/static/". For matching paths, we strip the
@@ -14,5 +14,5 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return mux
+	return secureHeaders(mux)
 }
