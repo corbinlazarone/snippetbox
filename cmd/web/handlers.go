bfc44e7e7a78e7674fbcd6dec93624d51bdae6cc
corbinlazarone <corbinlazarone1@gmail.com>
2025-09-27 01:24:25 -0500

    refractor: made package for validation helpers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,19 +6,19 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
-	"unicode/utf8"
 
 	"github.com/corbinlazarone/snippetbox/internal/models"
+	"github.com/corbinlazarone/snippetbox/internal/validator"
 	"github.com/julienschmidt/httprouter"
 )
 
 // NOTE: Struct field must be public in order to be read by
 // the html/template package when rendering the template.
 type snippetCreateForm struct {
-	Title       string
-	Content     string
-	Expires     int
-	FieldErrors map[string]string
+	Title   string
+	Content string
+	Expires int
+	validator.Validator
 }
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -69,36 +69,27 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 	}
 
 	form := snippetCreateForm{
-		Title:       r.PostForm.Get("title"),
-		Content:     r.PostForm.Get("content"),
-		Expires:     expires,
-		FieldErrors: map[string]string{}, // A map to hold any validation errors for the form fields
-
+		Title:   r.PostForm.Get("title"),
+		Content: r.PostForm.Get("content"),
+		Expires: expires,
 	}
 
-	// Check that the title field is not blank and is not more than 100
-	// characters long.
-	if strings.TrimSpace(form.Title) == "" {
-		form.FieldErrors["title"] = "This field cannot be blank"
-		// we use the utf8.RuneCountInString() b/c we want to count the number of characters
-	} else if utf8.RuneCountInString(form.Title) > 100 {
-		form.FieldErrors["title"] = "This field cannot be more than 100 characters long"
-	}
+	// Check that the title field is not blank.
+	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
+
+	// Check the the title field is not more than 100 characters long.
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
 
 	// Check that the content value isn't blank
-	if strings.TrimSpace(form.Content) == "" {
-		form.FieldErrors["content"] = "This field cannot be blank"
-	}
+	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
 
 	// Check that the expires value matches one of our permitted values (1, 7 or
 	// 365).
-	if form.Expires != 1 && form.Expires != 7 && form.Expires != 365 {
-		form.FieldErrors["expires"] = "This field must equal 1, 7 or 365"
-	}
+	form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
 
 	// If any errors in our map than re render the create.tmpl.html page
 	// with a 422 status code error.
-	if len(form.FieldErrors) > 0 {
+	if !form.Valid() {
 		data := app.newTemplateData()
 		data.Form = form
 		app.render(w, "create.tmpl.html", data, http.StatusUnprocessableEntity)
